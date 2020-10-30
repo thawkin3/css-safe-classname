6bfab3c9de3337cf22049096b6cbddc24b3826b7
Tyler Hawkins <thawkin3@byu.net>
2020-10-30 13:43:51 -0600

    test: adds extensive tests for createSafeCssClassname

--- src/create-safe-css-classname.js
+++ src/create-safe-css-classname.js
@@ -1,5 +1,20 @@
 import { nonCssSafeCharacters } from './non-css-safe-characters.js'
 
+// https://www.w3.org/TR/CSS2/syndata.html
+// Section 4.1.3 Characters and case
+
+// In CSS, identifiers can contain only:
+//   - the characters [a-zA-Z0-9]
+//   - ISO 10646 characters U+00A0 and higher
+//   - the hyphen (-)
+//   - the underscore (_)
+//   - escaped characters and any ISO 10646 character as a numeric code
+
+// CSS identifiers cannot start with:
+//   - a digit
+//   - two hyphens
+//   - a hyphen followed by a digit.
+
 export const createSafeCssClassname = str => {
   if (typeof str !== 'string') {
     return ''
--- src/create-safe-css-classname.test.js
+++ src/create-safe-css-classname.test.js
@@ -5,30 +5,160 @@ describe('createSafeCssClassname', () => {
     expect(typeof createSafeCssClassname('test')).toBe('string')
   })
 
-  it('removes spaces from the string', () => {
-    expect(createSafeCssClassname('This Is My Class')).toBe('ThisIsMyClass')
+  describe('invalid input', () => {
+    it('returns an empty string if the argument is not a string', () => {
+      expect(createSafeCssClassname(42)).toBe('')
+      expect(createSafeCssClassname(undefined)).toBe('')
+      expect(createSafeCssClassname(null)).toBe('')
+      expect(createSafeCssClassname([1, 2, 3])).toBe('')
+      expect(createSafeCssClassname({ key: 'value' })).toBe('')
+      expect(createSafeCssClassname(new Date())).toBe('')
+    })
   })
 
-  it('removes CSS-specific identifiers from the string', () => {
-    expect(createSafeCssClassname('.my#Class>Name~Is+Cool')).toBe(
-      'myClassNameIsCool'
-    )
+  describe('invalid characters', () => {
+    it('removes spaces from the string', () => {
+      expect(createSafeCssClassname('This Is My Class')).toBe('ThisIsMyClass')
+    })
+
+    it('strips out the ! character from the string', () => {
+      expect(createSafeCssClassname('cool!class')).toBe('coolclass')
+    })
+
+    it('strips out the " character from the string', () => {
+      expect(createSafeCssClassname('cool"class')).toBe('coolclass')
+    })
+
+    it('strips out the # character from the string', () => {
+      expect(createSafeCssClassname('cool#class')).toBe('coolclass')
+    })
+
+    it('strips out the $ character from the string', () => {
+      expect(createSafeCssClassname('cool$class')).toBe('coolclass')
+    })
+
+    it('strips out the % character from the string', () => {
+      expect(createSafeCssClassname('cool%class')).toBe('coolclass')
+    })
+
+    it('strips out the & character from the string', () => {
+      expect(createSafeCssClassname('cool&class')).toBe('coolclass')
+    })
+
+    it("strips out the ' character from the string", () => {
+      expect(createSafeCssClassname("cool'class")).toBe('coolclass')
+    })
+
+    it('strips out the ( character from the string', () => {
+      expect(createSafeCssClassname('cool(class')).toBe('coolclass')
+    })
+
+    it('strips out the ) character from the string', () => {
+      expect(createSafeCssClassname('cool)class')).toBe('coolclass')
+    })
+
+    it('strips out the * character from the string', () => {
+      expect(createSafeCssClassname('cool*class')).toBe('coolclass')
+    })
+
+    it('strips out the + character from the string', () => {
+      expect(createSafeCssClassname('cool+class')).toBe('coolclass')
+    })
+
+    it('strips out the , character from the string', () => {
+      expect(createSafeCssClassname('cool,class')).toBe('coolclass')
+    })
+
+    it('strips out the . character from the string', () => {
+      expect(createSafeCssClassname('cool.class')).toBe('coolclass')
+    })
+
+    it('strips out the / character from the string', () => {
+      expect(createSafeCssClassname('cool/class')).toBe('coolclass')
+    })
+
+    it('strips out the : character from the string', () => {
+      expect(createSafeCssClassname('cool:class')).toBe('coolclass')
+    })
+
+    it('strips out the ; character from the string', () => {
+      expect(createSafeCssClassname('cool;class')).toBe('coolclass')
+    })
+
+    it('strips out the < character from the string', () => {
+      expect(createSafeCssClassname('cool<class')).toBe('coolclass')
+    })
+
+    it('strips out the = character from the string', () => {
+      expect(createSafeCssClassname('cool=class')).toBe('coolclass')
+    })
+
+    it('strips out the > character from the string', () => {
+      expect(createSafeCssClassname('cool>class')).toBe('coolclass')
+    })
+
+    it('strips out the ? character from the string', () => {
+      expect(createSafeCssClassname('cool?class')).toBe('coolclass')
+    })
+
+    it('strips out the @ character from the string', () => {
+      expect(createSafeCssClassname('cool@class')).toBe('coolclass')
+    })
+
+    it('strips out the [ character from the string', () => {
+      expect(createSafeCssClassname('cool[class')).toBe('coolclass')
+    })
+
+    it('strips out the  character from the string', () => {
+      expect(createSafeCssClassname('coolclass')).toBe('coolclass')
+    })
+
+    it('strips out the ] character from the string', () => {
+      expect(createSafeCssClassname('cool]class')).toBe('coolclass')
+    })
+
+    it('strips out the ^ character from the string', () => {
+      expect(createSafeCssClassname('cool^class')).toBe('coolclass')
+    })
+
+    it('strips out the ` character from the string', () => {
+      expect(createSafeCssClassname('cool`class')).toBe('coolclass')
+    })
+
+    it('strips out the { character from the string', () => {
+      expect(createSafeCssClassname('cool{class')).toBe('coolclass')
+    })
+
+    it('strips out the | character from the string', () => {
+      expect(createSafeCssClassname('cool|class')).toBe('coolclass')
+    })
+
+    it('strips out the } character from the string', () => {
+      expect(createSafeCssClassname('cool}class')).toBe('coolclass')
+    })
+
+    it('strips out the ~ character from the string', () => {
+      expect(createSafeCssClassname('cool~class')).toBe('coolclass')
+    })
+
+    it('removes all instances of invalid characters, not just the first instance', () => {
+      expect(createSafeCssClassname('.my#Class>Name~Is+Cool')).toBe(
+        'myClassNameIsCool'
+      )
+    })
   })
 
-  it('does not affect a valid class name string', () => {
-    expect(createSafeCssClassname('cool')).toBe('cool')
-    expect(createSafeCssClassname('Cool')).toBe('Cool')
-    expect(createSafeCssClassname('coolClass')).toBe('coolClass')
-    expect(createSafeCssClassname('CoolClass')).toBe('CoolClass')
-    expect(createSafeCssClassname('cool-class')).toBe('cool-class')
-    expect(createSafeCssClassname('Cool-Class')).toBe('Cool-Class')
-  })
-
-  it('returns an empty string if the argument is not a string', () => {
-    expect(createSafeCssClassname(42)).toBe('')
-    expect(createSafeCssClassname(undefined)).toBe('')
-    expect(createSafeCssClassname(null)).toBe('')
-    expect(createSafeCssClassname([1, 2, 3])).toBe('')
-    expect(createSafeCssClassname({ key: 'value' })).toBe('')
+  describe('valid characters', () => {
+    it('does not affect a valid class name string', () => {
+      expect(createSafeCssClassname('cool')).toBe('cool')
+      expect(createSafeCssClassname('Cool')).toBe('Cool')
+      expect(createSafeCssClassname('coolClass')).toBe('coolClass')
+      expect(createSafeCssClassname('CoolClass')).toBe('CoolClass')
+      expect(createSafeCssClassname('cool-class')).toBe('cool-class')
+      expect(createSafeCssClassname('Cool-Class')).toBe('Cool-Class')
+      expect(createSafeCssClassname('_cool')).toBe('_cool')
+      expect(createSafeCssClassname('-cool')).toBe('-cool')
+      expect(createSafeCssClassname('cool123')).toBe('cool123')
+    })
   })
 })
